2b287b76e46e2732a988f79d71ce217cef28e34a
Thanningham <aethaniel.nazareno.22@gmail.com>
2026-07-11 00:06:09 +0800

    feat: implement calculator logic

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
-let firstNum = 0;
-let secondNum = 0;
-let operator;
+const buttonsContainer = document.querySelector(".buttons-container");
+const displayText = document.querySelector(".display-text");
+const OPERATORS = "+-/*"
+const NUMBERS = "0987654321"
+let displayedNum = "";
+let storedNum = null;
+let shouldClearDisplay = false;
+let currentOperator;
+let calculatorIsShowingResult = false;
+
+
+function updateStoredNum(num) {
+    storedNum = Number(num);
+}
+
+function resetCalculator() {
+    displayedNum = ""
+    storedNum = null;
+    shouldClearDisplay = false;
+    currentOperator = null;
+    calculatorIsShowingResult = false;
+}
+function clear() {
+    resetCalculator();
+    updateDisplay();
+}
+
+function updateDisplay() {
+    displayText.textContent = displayedNum;
+}
+
+function clearDisplay() {
+    displayedNum = "";
+    shouldClearDisplay = false;
+    updateDisplay();
+}
 
 function operate(operator, a, b) {
-    a = Number(a);
-    b = Number(b);
     let result = 0;
 
     if(operator === "+") {
@@ -41,3 +72,80 @@ function divide(a, b) {
 function multiply(a, b) {
     return a * b;
 }
+
+function calculateResult() {
+    console.log(`calculated: ${storedNum} ${currentOperator} ${displayedNum} = `);
+    return operate(currentOperator, storedNum, Number(displayedNum));
+}
+
+function displayResult(result) {
+    displayedNum = String(result);
+    updateDisplay();
+}
+
+function handleNumber(num) {
+    if(shouldClearDisplay) {
+        clearDisplay();
+    }
+
+    if(calculatorIsShowingResult) {
+        resetCalculator();
+    }
+
+    displayedNum += num;
+    updateDisplay();
+    console.log(`current stored num ${storedNum}`);
+}
+
+function handleOperator(operator) {
+
+    if(storedNum !== null && !calculatorIsShowingResult) {
+        const result = calculateResult();
+        updateStoredNum(result);
+        displayResult(result);
+    } else {
+        updateStoredNum(displayedNum);
+        calculatorIsShowingResult = false;
+    }
+    
+    currentOperator = operator;
+    shouldClearDisplay = true;
+    console.log(`operator pressed: ${currentOperator}`);
+    console.log(`current stored num ${storedNum}`);
+}
+
+function handleEquals() {
+    if (storedNum === null) {
+        return;
+    }
+
+    const result = calculateResult();
+    updateStoredNum(result);
+    displayResult(result);
+
+    shouldClearDisplay = true;
+    calculatorIsShowingResult = true;
+    
+    console.log(`current stored num ${storedNum}`);
+    console.log(`current displayeed num ${displayedNum}`);
+}
+
+function handleButtonClick(event) {
+    if(event.target.tagName !== "BUTTON") return;
+    
+    const buttonText = event.target.textContent;
+    
+    if (NUMBERS.includes(buttonText)) {
+        handleNumber(buttonText);
+    } else if (buttonText== 'clear') {
+        clear()
+    } else if (OPERATORS.includes(buttonText)) {
+        handleOperator(buttonText);
+    } else if (buttonText === "=") {
+        handleEquals()
+    }
+}
+
+buttonsContainer.addEventListener("click", handleButtonClick);
+
+
